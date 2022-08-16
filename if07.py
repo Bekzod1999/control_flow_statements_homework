af8cef0c466a57152d3c9cfcc40e708c981dd4d3
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-16 11:15:11 +0500

    07

--- if07.py
+++ if07.py
@@ -12,4 +12,17 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a==0:
+        a ='the number is zero'
+    elif a<0:
+        if a%2:
+            a = 'negative odd number'
+        else:
+            a = 'negative even number'
+    elif a%2:
+        a = 'positive odd number'
+    else:
+        a = 'positive even number'
+    return a
+x=main(10)
+print(x)
