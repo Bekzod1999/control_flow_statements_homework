e500c3190c1562db50a161ea46242643323b3457
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-16 11:15:38 +0500

    09

--- if09.py
+++ if09.py
@@ -9,4 +9,16 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    #12 and 21 false
+    #75 and 57 true
+    #11 and 11 true
+    y = a%10 #7
+    z = a//10 #5
+    w = y * 10 +z
+    if a >= w:
+        a = 'True'
+    else: 
+        a='False'
+    return a
+x=main(11)
+print(x)
