26a0d97d08f469ceb288f724a309b5b15adb9269
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-16 10:51:18 +0500

    08

--- if08.py
+++ if08.py
@@ -11,4 +11,16 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    if a > 99 or a < -99:
+        if a % 2:
+            a = 'three-digit odd number'
+        else:
+            a = 'three-digit even number'
+    elif a < 99 and a > -100:
+        if a%2:
+            a = 'two-digit odd number'
+        else:
+            a = 'two-digit even number'
+    return a
+x = main(0)
+print(x)
