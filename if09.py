339d4ee188d14ecf623df11ec24770a2eeda9d7e
Bekzod1999 <developmentfirm1999@gmail.com>
2022-08-16 11:29:58 +0500

    009

--- if09.py
+++ if09.py
@@ -15,10 +15,4 @@ def main(a):
     y = a%10 #7
     z = a//10 #5
     w = y * 10 +z
-    if a >= w:
-        a = 'True'
-    else: 
-        a='False'
-    return a
-x=main(11)
-print(x)
+    return a >= w
